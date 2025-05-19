dee7a706e16edcb70a2198a4fe4e54e0baf90752
Domino117204 <dominik.fischer@student.put.poznan.pl>
2025-05-19 22:36:22 +0200

    Poprawka main

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 import sys
 from graph import Graph
-from algorithms import find_euler_cycle, find_hamilton_cycle
+from export import export_to_tex
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: ./program --hamilton | --non-hamilton")
+    if len(sys.argv) < 2 or sys.argv[1] not in ['--hamilton', '--non-hamilton']:
+        print("Użycie: python main.py --hamilton | --non-hamilton")
         return
 
-    mode = sys.argv[1]
     n = int(input("nodes> "))
-    saturation = int(input("saturation> ")) if mode == '--hamilton' else 50
-
-    graph = Graph(n)
-
-    if mode == '--hamilton':
-        graph.generate_hamiltonian_graph(saturation)
-    elif mode == '--non-hamilton':
-        graph.generate_non_hamiltonian_graph()
-    else:
-        print("Invalid mode")
+    if n <= 10:
+        print("Liczba wierzchołków musi być większa niż 10.")
         return
+
+    if sys.argv[1] == '--hamilton':
+        saturation = int(input("saturation (30 or 70)> "))
+        if saturation not in [30, 70]:
+            print("Saturacja musi być 30 lub 70.")
+            return
+        graph = Graph(n)
+        graph.generate_hamiltonian_graph(saturation_percent=saturation)
+
+    elif sys.argv[1] == '--non-hamilton':
+        graph = Graph(n)
+        graph.generate_non_hamiltonian_graph()
 
     graph.print_graph()
-
-    print("\nEuler cycle:", find_euler_cycle(graph))
-    print("Hamilton cycle:", find_hamilton_cycle(graph))
+    export_to_tex(graph, "graph_output.tex")
 
 if __name__ == "__main__":
     main()
